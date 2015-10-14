56070881969891e52523640a3964becdf98e692d
pebefran <perezbenetf@gmail.com>
2015-10-14 12:06:22 +0200

    Initial Version

---
+++ src/org/japo/java/basics/samples/BienvenidoMundo.java
@@ -0,0 +1,12 @@
+
+package org.japo.java.basics.samples;
+
+public class BienvenidoMundo {
+
+        public static void main(String[] args) {
+        
+            System.out.println("Bienvenido Mundo");
+                    
+    }
+    
+}
